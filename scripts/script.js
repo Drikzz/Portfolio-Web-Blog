e81912b162976994af8971a8aa0d9cd82effb373
Drikzz <rikzsuarez@gmail.com>
2025-06-22 16:12:00 +0800

    please push

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,26 @@
-const mobileNav = document.querySelector('.mobile-nav');
 const hamburger = document.querySelector('.fa-bars');
+const mobileNav = document.querySelector('.mobile-nav');
 const navBlur = document.querySelector('.nav-blur');
-const body = document.body;
+const navLinks = document.querySelectorAll('.nav-bar a');
+const wholeContainer = document.querySelector('.whole-container');
 
 function openMobileNav() {
   
   mobileNav.classList.toggle('active');
   hamburger.classList.toggle('active');
-  body.classList.toggle('no-scroll');
+  wholeContainer.classList.toggle('no-scroll');
 }
 
 navBlur.addEventListener('click', () => {
   mobileNav.classList.remove('active');
   hamburger.classList.remove('active');
-  body.classList.remove('no-scroll');
+  wholeContainer.classList.remove('no-scroll');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    mobileNav.classList.remove('active');
+    hamburger.classList.remove('active');
+    wholeContainer.classList.remove('no-scroll');
+  })
 });
